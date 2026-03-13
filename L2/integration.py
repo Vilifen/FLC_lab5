246c62361592d018bb4b751c66e375cc32e8f58d
Vilifen <packliien@gmail.com>
2026-03-14 06:08:23 +0700

    color

--- L2/integration.py
+++ L2/integration.py
@@ -1,6 +1,5 @@
 from .scanner import Scanner
 from .parser import Parser
-from .results_table import build_table_rows
 
 
 def run_scanner(editor):
@@ -13,6 +12,44 @@ def run_scanner(editor):
     syntax_errors = parser.parse()
 
     all_errors = lex_errors + syntax_errors
-    token_rows, error_rows = build_table_rows(tokens, all_errors)
+
+    token_rows = []
+    error_rows = []
+
+    for t in tokens:
+        token_rows.append({
+            "code": t.type.value,
+            "type": t.type.name,
+            "lexeme": t.value,
+            "location": f"строка {t.line}, {t.column}",
+            "line": t.line,
+            "col": t.column
+        })
+
+    for e in all_errors:
+        already_in_tokens = any(
+            t.line == e.line and t.column == e.column
+            for t in tokens
+        )
+
+        if not already_in_tokens:
+            row = {
+                "code": e.code,
+                "type": "недопустимый символ",
+                "lexeme": e.char,
+                "location": f"строка {e.line}, {e.column}",
+                "line": e.line,
+                "col": e.column
+            }
+            token_rows.append(row)
+
+        error_rows.append({
+            "code": e.code,
+            "type": "недопустимый символ",
+            "lexeme": e.char,
+            "location": f"строка {e.line}, {e.column}",
+            "line": e.line,
+            "col": e.column
+        })
 
     return token_rows, error_rows
