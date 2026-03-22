5e6586c71ee742ae360bcfb6b2ffd80facfa6fdd
Vilifen <packliien@gmail.com>
2026-03-22 22:26:40 +0700

    Iron3

--- L2/parser.py
+++ L2/parser.py
@@ -65,7 +65,6 @@ class Parser:
         self._skip_ws()
         if not self._accept(TokenType.KEYWORD, "while"):
             self._error_with_irons("Ожидалось ключевое слово 'while'")
-            return
 
         self._skip_ws()
         if not self._accept(TokenType.SEPARATOR, "("):
@@ -156,153 +155,18 @@ class Parser:
         tok = self.tokens[self.pos]
         if tok.type == ttype and (value is None or tok.value == value):
             self.pos += 1
+            if self.stack.top():
+                self.stack.top()["position"] += 1
             return True
         return False
 
     def _error_with_irons(self, msg):
-        tok = self.tokens[self.pos] if not self._eof() else None
-        if tok is None:
-            return
-
-        code = ERROR_CODES["INVALID_STRUCTURE"]
-        self.errors.append(ScanError(code, msg, tok.line, tok.column, tok.value))
-
-        L = self._build_L()
-
-        current_symbol = tok.value
-        while not self._eof() and current_symbol not in L:
-            self.pos += 1
-            if not self._eof():
-                current_symbol = self.tokens[self.pos].value
-
         if self._eof():
             return
-
-        incomplete_node = self._find_incomplete_node(current_symbol)
-
-        if incomplete_node:
-            q = self._generate_q(incomplete_node)
-            if q:
-                self.tokens[self.pos:self.pos] = q
-
-    def _build_L(self):
-        L = set()
-        stack_copy = self.stack.stack.copy()
-
-        while stack_copy:
-            node = stack_copy.pop()
-            if node["type"] in self.follow_sets:
-                L.update(self.follow_sets[node["type"]])
-
-            production = node["production"]
-            pos = node.get("position", 0)
-
-            for i in range(pos, len(production)):
-                symbol = production[i]
-                if symbol in self.first_sets:
-                    L.update(self.first_sets[symbol])
-                elif isinstance(symbol, str) and not symbol.startswith("$"):
-                    L.add(symbol)
-
-        return L
-
-    def _find_incomplete_node(self, current_symbol):
-        for i in range(len(self.stack.stack) - 1, -1, -1):
-            node = self.stack.stack[i]
-            production = node["production"]
-            pos = node.get("position", 0)
-
-            for j in range(pos, len(production)):
-                symbol = production[j]
-
-                if symbol == current_symbol:
-                    return node
-
-                if symbol in self.first_sets and current_symbol in self.first_sets[symbol]:
-                    return node
-
-                if isinstance(symbol, str) and not symbol.startswith("$"):
-                    if self._can_derive(symbol, current_symbol):
-                        return node
-
-        if self.stack.stack:
-            return self.stack.stack[-1]
-        return None
-
-    def _can_derive(self, nonterminal, terminal):
-        if nonterminal == terminal:
-            return True
-
-        if nonterminal in self.first_sets:
-            return terminal in self.first_sets[nonterminal]
-
-        return False
-
-    def _generate_q(self, incomplete_node):
-        q = []
-        production = incomplete_node["production"]
-        pos = incomplete_node.get("position", 0)
-
-        needed = []
-        for i in range(pos, len(production)):
-            symbol = production[i]
-            if isinstance(symbol, str) and not symbol.startswith("$"):
-                needed.append(symbol)
-            elif symbol in self.first_sets:
-                needed.append(symbol)
-
-        for symbol in needed:
-            q.extend(self._expand_to_terminals(symbol))
-
-        return q
-
-    def _expand_to_terminals(self, symbol):
-        result = []
-
-        if symbol == "$id":
-            result.append(self._make_fake("IDENTIFIER", "$i"))
-        elif symbol == "$num":
-            result.append(self._make_fake("NUMBER", "0"))
-        elif symbol == "relop":
-            result.append(self._make_fake("OPERATOR", "=="))
-        elif symbol == "++":
-            result.append(self._make_fake("OPERATOR", "++"))
-        elif symbol == "--":
-            result.append(self._make_fake("OPERATOR", "--"))
-        elif symbol == ";":
-            result.append(self._make_fake("SEPARATOR", ";"))
-        elif symbol == "{":
-            result.append(self._make_fake("SEPARATOR", "{"))
-        elif symbol == "}":
-            result.append(self._make_fake("SEPARATOR", "}"))
-        elif symbol == "(":
-            result.append(self._make_fake("SEPARATOR", "("))
-        elif symbol == ")":
-            result.append(self._make_fake("SEPARATOR", ")"))
-        elif symbol == "&&":
-            result.append(self._make_fake("OPERATOR", "&&"))
-        elif symbol == "||":
-            result.append(self._make_fake("OPERATOR", "||"))
-        elif symbol == "while":
-            result.append(self._make_fake("KEYWORD", "while"))
-        elif symbol in self.productions:
-            production = self.productions[symbol]
-            if isinstance(production, list):
-                first_prod = production[0] if production else []
-                for s in first_prod:
-                    result.extend(self._expand_to_terminals(s))
-
-        return result
-
-    def _make_fake(self, ttype, value):
-        class FakeTok:
-            def __init__(self, ttype, value):
-                self.type = getattr(TokenType, ttype)
-                self.value = value
-                self.line = -1
-                self.column = -1
-
-        return FakeTok(ttype, value)
+        tok = self.tokens[self.pos]
+        code = ERROR_CODES["INVALID_STRUCTURE"]
+        self.errors.append(ScanError(code, msg, tok.line, tok.column, tok.value))
+        self.pos += 1
 
     def _skip_ws(self):
         while not self._eof() and self.tokens[self.pos].type == TokenType.WHITESPACE:
